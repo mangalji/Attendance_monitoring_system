13a8230610e027f8c7edf3f2dc4e7b2f1fe4505b
mangalji <rajmangal03@gmail.com>
2026-01-08 16:34:43 +0530

    models added

---
+++ accounts/models.py
@@ -0,0 +1,81 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+class Domain(models.Model):
+    domain_name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.domain_name
+
+class Manager(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(max_length=100)
+    department = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.email})"
+    
+class Student(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    roll_no = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=100)
+    # email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    city = models.CharField(max_length=50, blank=True,null=True)
+    address = models.TextField(blank=True, null=True)
+    pincode = models.CharField(blank=True,null=True)
+    dob = models.DateField()
+    last_qualification = models.CharField(max_length=100, blank=True,null=True)
+    domain = models.ForeignKey(Domain, on_delete=models.SET_NULL,null=True)
+    joining_date = models.DateField()
+    photo = models.ImageField(upload_to='student_photos/',blank=True,null=True)
+    is_active = models.BooleanField(default=True)
+    added_by = models.ForeignKey(Manager, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"{self.name} ({self.roll_no})"
+    
+class Parent(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)  # direct student_id
+    name = models.CharField(max_length=100)
+    email = models.EmailField(blank=True, null=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    relation = models.CharField(max_length=50, default="Father")  # default father
+
+    def __str__(self):
+        return f"{self.name} ({self.relation})"
+    
+
+    
+class Company(models.Model):
+    company_name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100, blank=True, null=True)
+    
+    def __str__(self):
+        return self.company_name
+
+        
+class Placement(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True)
+    placed_date = models.DateField()
+
+    class Meta:
+        unique_together = ('student', 'company', 'placed_date')
+
+    def __str__(self):
+        return f"{self.student.name} ({self.company.company_name})"
