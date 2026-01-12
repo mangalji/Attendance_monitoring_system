0fee96368e920b0eb19cf0ea8622b2b9fcc08a87
mangalji <rajmangal03@gmail.com>
2026-01-12 11:42:36 +0530

    some error occurs so i deleted complete project

--- accounts/urls/student_urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from accounts.views.student_views import student_dashboard, student_profile, edit_student_profile
-
-urlpatterns = [
-    path("dashboard/", student_dashboard, name="student_dashboard"),
-    path("profile/", student_profile, name="student_profile"),
-    path("profile/edit/",edit_student_profile,name="edit_student_profile")
-]
--- attendance_system/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AttendanceSystemConfig(AppConfig):
-    name = 'attendance_system'
