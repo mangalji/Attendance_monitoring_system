4f67098663c7a765ad82d8174f13bce0a4c64900
mangalji <rajmangal03@gmail.com>
2026-01-12 13:23:52 +0530

    added functionality for admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ManagerProfile, StudentProfile, Parent, Placement,Company
+from .forms import ManagerCretionForm
+
+@admin.register(ManagerProfile)
+class ManagerAdmin(admin.ModelAdmin):
+    list_display = ('id','user','phone')
+
+    def get_form(self,request,obj=None,**kwargs):
+        if obj is None:
+            return ManagerCretionForm
+        return super().get_form(request,obj,**kwargs)
+    
+
+@admin.register(StudentProfile)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('roll_no','user','domain','joining_date','is_placed','is_active')
+    list_filter = ('domain','is_placed','is_active')
+    search_fields = ('roll_no','user__first_name','user__email')
+
+@admin.register(Parent)
+class ParentAdmin(admin.ModelAdmin):
+    list_display = ('student','name','relation','phone')
