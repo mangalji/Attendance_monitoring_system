4c39b9ea2c5b44238868f0bcb2e68f93d04d1db8
mangalji <rajmangal03@gmail.com>
2026-01-24 10:59:58 +0530

    code fix

--- fees/templatetags/fees_extras.py
+++ fees/templatetags/fees_extras.py
@@ -5,8 +5,4 @@ register = template.Library()
 
 @register.filter
 def get_attr(obj, attr_name):
-    """
-    Returns the value of an attribute of an object.
-    Usage: {{ object|get_attr:"attribute_name" }}
-    """
     return getattr(obj, attr_name, None)
