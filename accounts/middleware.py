1208101793c718edaf38f08861bf77847f8f1519
mangalji <rajmangal03@gmail.com>
2026-02-05 17:54:34 +0530

    increase the time of session

--- accounts/middleware.py
+++ accounts/middleware.py
@@ -3,5 +3,5 @@ from django.utils.deprecation import MiddlewareMixin
 class SessionTimeoutMiddleware(MiddlewareMixin):
     def process_request(self,request):
         if request.user.is_authenticated:
-            request.session.set_expiry(300)# for 5 minutes == 300 seconds
+            request.session.set_expiry(30000)# for 500 minutes == 30000 seconds
         return None
